90430a38b15907d8bf8f4387a8924f5afbd4fcee
Peter Friedrich <peter.friedrich.one@gmail.com>
2025-07-15 18:56:18 -0700

    added init files to src and test

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Outlook email catcher
+
+Scans the junk folder to grab emails missclassified as spam
+"""
+
+__version__ = "0.1.0"
+__author__ = "Peter Friedrich"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+"""
+Tests for email catcher/junk scanner
+"""
