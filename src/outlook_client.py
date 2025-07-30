aa7b045072bc58c84f4c181158276df51d9140f4
Peter Friedrich <peter.friedrich.one@gmail.com>
2025-07-30 15:52:52 -0600

    Finished problems with device code request
    - added except block
    - fixed sytax errors
    - added returns

--- src/outlook_client.py
+++ src/outlook_client.py
@@ -21,26 +21,33 @@ class GraphAuthenticator:
         Return bool, true for success, else false.
         """
 
-		# make request urls
-		device_code_url = f"https://login.microsoftonline.com/{self.tenant_id}/oauth2/v2.0/devicecode"
-    	token_url = f"https://login.microsoftonline.com/{self.tenant_id}/oauth2/v2.0/token"
+        # make request urls
+        device_code_url = f"https://login.microsoftonline.com/{self.tenant_id}/oauth2/v2.0/devicecode"
+        token_url = f"https://login.microsoftonline.com/{self.tenant_id}/oauth2/v2.0/token"
 
-		# request device code, verification info
-		device_data = {
-			'client_id': self.client_id,
-			'scope':'https://graph.microsoft.com/Mail.Read https://graph.microsoft.com/Mail.ReadWrite'
-		}
-		
-		try:
-			device_response = requests.requests.post(device_code_url, data=device_data)
-			device_response.raise_for_status()
-			device_info = device_response.json()
-			
-			# prompt user to complete browser authentication
-			print(f"\n Authentication required:")
-			print(f"Go to: {device_info['verification_uri']}")
-			print(f"Enter code: {device_info['user_code']}")
-			print(f"Code expires in {device_info['expires_in']} seconds")	
+        # request device code, verification info
+        device_data = {
+            'client_id': self.client_id,
+            'scope':'https://graph.microsoft.com/Mail.Read https://graph.microsoft.com/Mail.ReadWrite'
+        }
+        
+        try:
+            device_response = requests.requests.post(device_code_url, data=device_data)
+            device_response.raise_for_status()
+            device_info = device_response.json()
+            
+            # prompt user to complete browser authentication
+            print(f"\n Authentication required:")
+            print(f"Go to: {device_info['verification_uri']}")
+            print(f"Enter code: {device_info['user_code']}")
+            print(f"Code expires in {device_info['expires_in']} seconds")
+
+        except Exception as e:
+            print(f"Authentication error: {e}")
+            return False
+
+        return False
+
 # basic test
 if __name__ == "__main__":
     auth = GraphAuthenticator()
@@ -49,5 +56,3 @@ if __name__ == "__main__":
         print("Authentication successful!")
     else:
         print("Authentication failed")
-
-
